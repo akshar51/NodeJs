54a7d052363de6ce40b83acb8db19d173c7c3186
akshar51 <aksharparekh401@gmail.com>
2025-07-16 16:07:26 +0530

    done

---
+++ Module,REPL/index.js
@@ -0,0 +1,8 @@
+const msg = require("./modules/msg");
+
+
+// console.log(__filename) // current file
+// console.log(__dirname) // current folder
+
+console.log(msg)
+// console.log(msg.print())
---
+++ Module,REPL/modules/msg.js
@@ -0,0 +1,8 @@
+const msg = {
+    companyName : "abc company",
+    address : "Navsari",
+    print : function(){
+        console.log(this.address)
+    },
+}
+module.exports = msg;
